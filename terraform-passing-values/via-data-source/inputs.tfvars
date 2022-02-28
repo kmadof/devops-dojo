2a894c714e0ed53450e125361515bc87045cee13
Krzysztof Madej <krzysztof.madej.1987+wsl@gmail.com>
2022-02-28 12:22:10 +0100

    Changed terraform scripts

--- terraform-passing-values/via-data-source/inputs.tfvars
+++
@@ -1,4 +0,0 @@
-subnet_sql_address_prefixes = "#{subnet_sql_address_prefixes}#"
-subnet_sql_managed_address_prefixes = "#{subnet_sql_managed_address_prefixes}#"
-subnet_web_address_prefixes = "#{subnet_web_address_prefixes}#"
-vnet_address_space = "#{vnet_address_space}#"
--- terraform-passing-values/via-data-source/variables.tf
+++ terraform-passing-values/via-data-source/variables.tf
@@ -1,19 +1,3 @@
 variable "env" {
   type = string
 }
-
-variable "vnet_address_space" {
-  type = string
-}
-
-variable "subnet_sql_managed_address_prefixes" {
-  type = string
-}
-
-variable "subnet_sql_address_prefixes" {
-  type = string
-}
-
-variable "subnet_web_address_prefixes" {
-  type = string
-}
