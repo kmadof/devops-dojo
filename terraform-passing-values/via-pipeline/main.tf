c833631c0d18035963d0e0e85ad742615dcf9e14
Krzysztof Madej <krzysztof.madej.1987+wsl@gmail.com>
2022-02-28 12:16:23 +0100

    Changed terraform scripts

--- terraform-passing-values/via-pipeline/main.tf
+++ terraform-passing-values/via-pipeline/main.tf
@@ -13,10 +13,10 @@ module "vnet" {
   name                = "shilda-${var.env}-vnet"
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
-  address_space       = [var.subnet_sql_address_prefixes]
+  address_space       = [var.vnet_address_space]
   subnets = [
     { name = "subnet-sql-managed", address_prefixes = [var.subnet_sql_managed_address_prefixes], delegation = "Microsoft.Sql/managedInstances" },
-    { name = "subnet-sql", address_prefixes = [var.subnet_web_address_prefixes], service_endpoints = ["Microsoft.Sql"] },
-    { name = "subnet-web", address_prefixes = [var.vnet_address_space], service_endpoints = ["Microsoft.Storage", "Microsoft.Web"], delegation = "Microsoft.Web/serverFarms" }
+    { name = "subnet-sql", address_prefixes = [var.subnet_sql_address_prefixes], service_endpoints = ["Microsoft.Sql"] },
+    { name = "subnet-web", address_prefixes = [var.subnet_web_address_prefixes], service_endpoints = ["Microsoft.Storage", "Microsoft.Web"], delegation = "Microsoft.Web/serverFarms" }
   ]
 }
